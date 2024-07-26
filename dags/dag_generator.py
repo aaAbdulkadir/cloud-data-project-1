65c1d1ea19db10aed9a8a0ba8eb75648df6714d4
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-07-27 00:11:13 +0100

    test dynamic with loop

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -112,3 +112,16 @@ def create_dag(scrape_dir_path: str) -> DAG:
 
 
     return dag
+
+
+dags_folder = os.path.dirname(os.path.abspath(__name__))
+
+for folder in os.listdir(dags_folder):
+    folder_path = os.path.join(dags_folder, folder)
+    
+    if os.path.isdir(folder_path):
+        yaml_file_path = os.path.join(folder_path, f'{folder}.yml')
+
+        if os.path.isfile(yaml_file_path):
+            dag = create_dag(folder_path)
+            globals()[dag.dag_id] = dag
