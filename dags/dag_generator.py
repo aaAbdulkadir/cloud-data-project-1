c2d7a16d6bea311714a751862f1902ab779149e0
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-07-27 00:49:33 +0100

    dynamic dag test

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -110,18 +110,4 @@ def create_dag(scrape_dir_path: str) -> DAG:
             for dependency in dependencies:
                 tasks[dependency] >> tasks[task_id]
 
-
     return dag
-
-
-dags_folder = os.path.dirname(os.path.abspath(__name__))
-
-for folder in os.listdir(dags_folder):
-    folder_path = os.path.join(dags_folder, folder)
-    
-    if os.path.isdir(folder_path):
-        yaml_file_path = os.path.join(folder_path, f'{folder}.yml')
-
-        if os.path.isfile(yaml_file_path):
-            dag = create_dag(folder_path)
-            globals()[dag.dag_id] = dag
