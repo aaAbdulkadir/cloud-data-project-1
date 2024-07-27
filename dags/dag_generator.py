926e90673dbc8926dad96fd02604a5c6fbcf2913
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-07-27 13:58:16 +0100

    fix jinja template params

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -79,12 +79,12 @@ def create_dag(scrape_dir_path: str) -> DAG:
             if task_id == 'extract':
                 task_kwargs.update({
                     'url': dag_params.get('url'),
-                    'output_filename': '{{ dag.dag_id }}_extract_to_transform_{{ ts }}.{{ file_extension }}',
+                    'output_filename': '{{ dag.dag_id }}_extract_to_transform_{{ ts }}.{}'.format(file_extension),
                     'logical_timestamp': '{{ ts }}',
                 })
             elif task_id == 'transform':
                 task_kwargs.update({
-                    'input_filename': '{{ dag.dag_id }}_extract_to_transform_{{ ts }}.{{ file_extension }}',
+                    'input_filename': '{{ dag.dag_id }}_extract_to_transform_{{ ts }}.{}'.format(file_extension),
                     'output_filename': '{{ dag.dag_id }}_transform_to_load_{{ ts }}.csv',
                 })
             elif task_id == 'load':
