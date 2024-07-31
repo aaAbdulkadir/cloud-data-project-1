bf8c9d635fe9e0b1dba1dbfeac4bfa96583261b8
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-07-31 23:55:56 +0100

    params different approach

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -52,7 +52,7 @@ def load_config_if_exists(scrape_dir_path: str, dag_params: dict) -> dict:
 #         params = task_params['params']
 #     return params
 
-def task_wrapper(task_function, next_task_id, **kwargs):
+def task_wrapper(task_function, next_task_id, kwargs):
     ti = kwargs['ti']
     task_id = kwargs['task'].task_id
     dag_id = kwargs['dag'].dag_id
@@ -65,8 +65,8 @@ def task_wrapper(task_function, next_task_id, **kwargs):
         url = kwargs['url']
         logical_timestamp = kwargs['logical_timestamp']
         config = kwargs['config']
-        print(kwargs)
-        task_function(url=url, output_filename=output_filename, logical_timestamp=logical_timestamp, config=config, **kwargs)
+        params = kwargs.get('params', {})
+        task_function(url=url, output_filename=output_filename, logical_timestamp=logical_timestamp, config=config, params=params)
     elif task_id == 'load':
         dataset_name = kwargs['dataset_name']
         input_filename = kwargs['input_filename']
@@ -75,7 +75,8 @@ def task_wrapper(task_function, next_task_id, **kwargs):
         task_function(dataset_name=dataset_name, input_filename=input_filename, mode=mode, keyfields=keyfields)
     else:
         input_filename = kwargs['input_filename']
-        task_function(input_filename=input_filename, output_filename=output_filename, **kwargs)
+        params = kwargs.get('params', {})
+        task_function(input_filename=input_filename, output_filename=output_filename, params=params)
 
     ti.xcom_push(key='output_filename', value=output_filename)
 
