78a48f9dc311961a305142163558790b304fb7ae
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-07-31 19:20:56 +0100

    filepath fixed

--- dags/dag_generator_init.py
+++ dags/dag_generator_init.py
@@ -8,6 +8,7 @@ dags_folder = '/home/ubuntu/airflow/dags'
 for root, dirs, files in os.walk(dags_folder):
     for file in files:
         if file.endswith('.yml'):
-            scrape_dir_path = root  
+            scrape_dir_path = root
+            yml_file_path = os.path.join(scrape_dir_path, file)
             dag_id = os.path.basename(scrape_dir_path)
-            globals()[dag_id] = create_dag(scrape_dir_path)
+            globals()[dag_id] = create_dag(yml_file_path)
