787062efa5a3035593469a72bd2d2d70680bd343
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-08-01 15:16:09 +0100

    test

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -45,14 +45,6 @@ def load_config_if_exists(scrape_dir_path: str, dag_params: dict) -> dict:
         config = load_json_file(config_path)
     return config
 
-# def load_in_params(task_params: dict) -> dict:
-#     """Load additional parameters if they exist."""
-#     params = {}
-#     if 'params' in task_params:
-#         params = task_params['params']
-#     return params
-
- ## PARAMS ISSUE FIX
 def task_wrapper(task_function, next_task_id, **kwargs):
     ti = kwargs['ti']
     task_id = kwargs['task'].task_id
@@ -61,12 +53,13 @@ def task_wrapper(task_function, next_task_id, **kwargs):
     file_extension = kwargs['dag'].default_args['file_extension']
 
     output_filename = get_filename_template(dag_id, task_id, next_task_id, ts, file_extension)
+    remaining_kwargs = {k: v for k, v in kwargs.items() if k not in {'url', 'output_filename'}}
 
     if task_id == 'extract':
         url = kwargs['url']  
         logical_timestamp = kwargs['logical_timestamp']
         config = kwargs.get('config', {})
-        task_function(url=url, output_filename=output_filename, logical_timestamp=logical_timestamp, config=config, **kwargs)
+        task_function(url=url, output_filename=output_filename, logical_timestamp=logical_timestamp, config=config, **remaining_kwargs)
     elif task_id == 'load':
         dataset_name = kwargs['dataset_name']
         input_filename = kwargs['input_filename']
@@ -75,7 +68,7 @@ def task_wrapper(task_function, next_task_id, **kwargs):
         task_function(dataset_name=dataset_name, input_filename=input_filename, mode=mode, keyfields=keyfields)
     else:
         input_filename = kwargs['input_filename']
-        task_function(input_filename=input_filename, output_filename=output_filename, **kwargs)
+        task_function(input_filename=input_filename, output_filename=output_filename, **remaining_kwargs)
 
     ti.xcom_push(key='output_filename', value=output_filename)
 
@@ -122,7 +115,6 @@ def create_dag(yml_file_path: str) -> DAG:
                 python_callable = load_to_rds
             else:
                 python_callable = getattr(functions, task_params.get('python_callable'))
-                # params = load_in_params(task_params)
 
             task_kwargs = {**task_params.get('params', {})}
 
@@ -134,7 +126,6 @@ def create_dag(yml_file_path: str) -> DAG:
                     'output_filename': get_filename_template(dag_id, task_id, next_task_id, '{{ ts }}', '{{ dag.default_args.file_extension }}'),
                     'logical_timestamp': '{{ ts }}',
                     'config': config,
-                    # 'params': params
                 })
             elif task_id == 'load':
                 previous_task_id = task_order[idx - 1]
@@ -149,13 +140,12 @@ def create_dag(yml_file_path: str) -> DAG:
                 task_kwargs.update({
                     'input_filename': "{{ ti.xcom_pull(task_ids='" + previous_task_id + "', key='output_filename') }}",
                     'output_filename': get_filename_template(dag_id, task_id, next_task_id, '{{ ts }}', '{{ dag.default_args.file_extension }}'),
-                    # 'params': params
                 })
 
             task = PythonOperator(
                 task_id=task_id,
                 python_callable=task_wrapper,
-                op_kwargs={'task_function': python_callable, 'next_task_id': next_task_id},
+                op_kwargs={'task_function': python_callable, 'next_task_id': next_task_id, **task_kwargs},
                 retries=task_params.get('retries', 0),
                 retry_delay=timedelta(seconds=task_params.get('retry_delay', 15)),
                 provide_context=True,
