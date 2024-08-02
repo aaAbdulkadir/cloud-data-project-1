2f447bbdf9eb4cf5fc4a42aefd06333d6e77f213
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-08-02 19:53:44 +0100

    old xcmon way

--- dags/dummy_dag/dummy_dag_functions.py
+++ dags/dummy_dag/dummy_dag_functions.py
@@ -3,7 +3,6 @@ def extract(
     output_filename: str,
     logical_timestamp: "pendulum.datetime",
     config: dict,
-    historical: bool,
 ) -> int:
     """Dummy extract for testing purposes.
 
@@ -19,7 +18,6 @@ def extract(
     logger.info(output_filename)
     logger.info(logical_timestamp)
     logger.info(config)
-    logger.info(historical)
 
     with open(output_filename, 'w') as f:
         f.write('Testing,1,animal')
