03c3fb1ffc8eb28dc5e7230e89ed3f5036041946
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-08-09 19:19:57 +0100

    params

--- dags/new_york_taxi/new_york_taxi_functions.py
+++ dags/new_york_taxi/new_york_taxi_functions.py
@@ -13,6 +13,8 @@ def extract(
     
     logger = logging.getLogger('extract')
     
+    logger.info(f'PARAMS: {params}')
+    
     taxi_type = params['taxi_type']
     logger.info(f'Formulating URL for {taxi_type}')
     url = formulate_url(url, taxi_type, logical_timestamp)
