18b0d9e9a7c067ba27b670cacb06788ec6c13676
Abdulkadir Abdulkadir <a.abdulkadir@hotmail.com>
2024-08-14 20:12:24 +0100

    historical dag new york taxi

--- dags/dag_generator_init.py
+++ dags/dag_generator_init.py
@@ -1,4 +1,5 @@
 import os 
+import logger
 
 from dag_generator import create_dag
 
@@ -11,5 +12,14 @@ for root, dirs, files in os.walk(dags_folder):
             scrape_dir_path = root
             yml_file_path = os.path.join(scrape_dir_path, file)
             dag_id = os.path.basename(scrape_dir_path)
-            globals()[dag_id] = create_dag(yml_file_path)
+            
+            try:
+                dag = create_dag(yml_file_path)
+                if dag:
+                    globals()[dag_id] = dag
+                    logger.info(f"Successfully loaded DAG: {dag_id}")
+                else:
+                    logger.warning(f"DAG creation returned None for {dag_id}")
+            except Exception as e:
+                logger.error(f"Error loading DAG {dag_id}: {str(e)}")
             
